2b70c3630d1495d59f65fefb42dc613bcb6859a6
Alexander Galtsev <alex.galtsev@tum.de>
2016-08-02 23:18:46 +0200

    parsePDB

--- src/main/java/com/rostlab/PDB/PDBEntity.java
+++ src/main/java/com/rostlab/PDB/PDBEntity.java
@@ -1,5 +1,9 @@
 package com.rostlab.PDB;
 
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.Chain;
+import org.biojava.nbio.structure.StructureIO;
+
 /**
  * Created by Longes on 26.05.2016.
  */
@@ -11,4 +15,25 @@ public class PDBEntity {
         this.acc_id = acc_id;
         this.dssp = dssp;
     }
+
+    public void parsePDB() {
+        try {
+
+            Structure s = StructureIO.getStructure(acc_id);
+
+            for ( Chain c : s.getChains()) {
+
+                // only the observed residues
+                System.out.println(c.getAtomSequence());
+
+                // print biological sequence
+                System.out.println(c.getSeqResSequence());
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
 }
