d4a8a76e59bac2ef20c6a2570b1f4498ca776a0a
Alexander Galtsev <alex.galtsev@tum.de>
2016-09-17 02:12:46 +0200

    requests

--- src/main/java/com/rostlab/requestModule/requestMain.java
+++ src/main/java/com/rostlab/requestModule/requestMain.java
@@ -1,10 +1,19 @@
 package com.rostlab.requestModule;
 
+import com.rostlab.requestModule.request.JpredRequester;
+
+import java.io.IOException;
+
 /**
  * Created by Longes on 15.09.2016.
  */
 public class requestMain {
-    public void jpredRequests() {
-
+    public void jpredRequest(String sequence) {
+        JpredRequester jpredRequester = new JpredRequester(email);
+        try {
+            jpredRequester.makeRequest(sequence);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
