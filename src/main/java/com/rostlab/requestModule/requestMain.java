4ca0a36a91737a784c8ae776b6bc5704b8157c40
Alexander Galtsev <alex.galtsev@tum.de>
2016-09-21 08:16:25 +0200

    requests

--- src/main/java/com/rostlab/requestModule/requestMain.java
+++ src/main/java/com/rostlab/requestModule/requestMain.java
@@ -1,6 +1,8 @@
 package com.rostlab.requestModule;
 
 import com.rostlab.requestModule.request.JpredRequester;
+import com.rostlab.requestModule.request.PSIPREDrequester;
+import com.rostlab.requestModule.request.RaptorXRequester;
 
 import java.io.IOException;
 
@@ -8,6 +10,9 @@ import java.io.IOException;
  * Created by Longes on 15.09.2016.
  */
 public class requestMain {
+
+    public String email = "alex.galtsev@tum.de";
+
     public void jpredRequest(String sequence) {
         JpredRequester jpredRequester = new JpredRequester(email);
         try {
@@ -15,5 +20,18 @@ public class requestMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void raptorXRequest(String sequence) {
+        RaptorXRequester raptorXRequester = new RaptorXRequester(email);
+        try {
+            raptorXRequester.makeRequest(sequence);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void psipredRequest(String sequence) {
+        PSIPREDrequester psipredRequester = new PSIPREDrequester(email);
     }
 }
