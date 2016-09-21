1d962aaa6d5dfdb547466e5b0e148cd8076893c9
Alexander Galtsev <alex.galtsev@tum.de>
2016-09-21 08:25:45 +0200

    requests

--- src/main/java/com/rostlab/generationModule/uniprot/UniProtDAO.java
+++ src/main/java/com/rostlab/generationModule/uniprot/UniProtDAO.java
@@ -2,6 +2,8 @@ package com.rostlab.generationModule.uniprot;
 
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
  * Created by Longes on 31.05.2016.
  */
@@ -10,6 +12,8 @@ public interface UniProtDAO {
     UniProtEntity findByAccId (@Param("acc_id") String acc_id);
 
     UniProtEntity findBySequence (@Param("sequence") String sequence);
+
+    List<UniProtEntity> findAll ();
 
     void update(@Param("entity") UniProtEntity uniProtEntity);
 
--- src/main/java/com/rostlab/requestModule/requestMain.java
+++ src/main/java/com/rostlab/requestModule/requestMain.java
@@ -1,15 +1,21 @@
 package com.rostlab.requestModule;
 
+import com.rostlab.generationModule.uniprot.UniProtDAO;
+import com.rostlab.generationModule.uniprot.UniProtEntity;
 import com.rostlab.requestModule.request.JpredRequester;
 import com.rostlab.requestModule.request.PSIPREDrequester;
 import com.rostlab.requestModule.request.RaptorXRequester;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Longes on 15.09.2016.
  */
 public class requestMain {
+
+    private UniProtDAO uniProtDAO;
 
     public String email = "alex.galtsev@tum.de";
 
@@ -33,5 +39,21 @@ public class requestMain {
 
     public void psipredRequest(String sequence) {
         PSIPREDrequester psipredRequester = new PSIPREDrequester(email);
+        psipredRequester.makeRequest(sequence);
+    }
+
+    public void requestAll() {
+        List<UniProtEntity> uniprots = uniProtDAO.findAll();
+        for (Iterator<UniProtEntity> i = uniprots.iterator(); i.hasNext(); ) {
+            UniProtEntity entity = i.next();
+            jpredRequest(entity.sequence);
+            raptorXRequest(entity.sequence);
+            psipredRequest(entity.sequence);
+            try {
+                wait(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
